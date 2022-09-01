d32cc60a18c76596ab323235ff271b7d79703dfb
Виктория Ахматова <akhmatova@erker-it.ru>
2022-09-02 03:07:30 +0400

    adding business section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+type Props = {
+  styles: string;
+  text: string;
+};
 
-type Props = {};
-
-const Button = (props: Props) => {
-  return <div>Button</div>;
+const Button = ({ styles, text }: Props) => {
+  return (
+    <button
+      type="button"
+      className={`font-poppins font-medium text-primary text-[18px] outline-none py-4 px-6 bg-blue-gradient ${styles}`}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
