55e3025951d5ad98102be12f0c03afb48528c365
Виктория Ахматова <akhmatova@erker-it.ru>
2022-09-03 21:35:06 +0400

    copytight symbol added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -50,7 +50,7 @@ const Footer = () => (
     </div>
     <div className="flex md:flex-row flex-col justify-between w-full items-center py-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright 2021 HooBank. All Rights Reserved.
+        Copyright &copy; 2021 HooBank. All Rights Reserved.
       </p>
       <div className="flex flex-row md:mt-0 mt-6">
         {socialMedia.map((social, idx) => (
